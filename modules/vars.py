3fcc93075d44b0d9835b538e81133014434505b6
Kewah26330 <185765445+Kewah26330@users.noreply.github.com>
2024-11-15 08:57:14 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,6 +1,6 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "23551823")
+API_ID    = os.environ.get("API_ID", "676580165")
 API_HASH  = os.environ.get("API_HASH", "36727c8db0a63181fe9963f605cc4ef4")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7951686075:AAF2IS_i-Ku8m29Z2qT7WiP_pM9OuP8URZk") 
 
